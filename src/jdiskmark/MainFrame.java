3da4d5d0106037c3456483dce8d0985795f9d641
James Mark Chan <jamesmarkchan@gmail.com>
2024-01-16 15:58:16 -0800

    revise sample size dropdown options

--- src/jdiskmark/MainFrame.java
+++ src/jdiskmark/MainFrame.java
@@ -209,8 +209,8 @@ public final class MainFrame extends javax.swing.JFrame {
         jLabel8.setText("No. Samples");
 
         numFilesCombo.setEditable(true);
-        numFilesCombo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "25", "50", "75", "100", "150", "200", "250", "300", "350", "400", "450", "500", "1000" }));
-        numFilesCombo.setSelectedIndex(5);
+        numFilesCombo.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "50", "100", "200", "300", "500", "1000", "2000", "3000", "5000", "10000" }));
+        numFilesCombo.setSelectedIndex(2);
         numFilesCombo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 numFilesComboActionPerformed(evt);
