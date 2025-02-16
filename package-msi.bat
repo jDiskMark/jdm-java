d505848652c4da213bc94d7c56078d7f16f994e3
James Mark Chan <jamesmarkchan@gmail.com>
2025-02-16 01:51:56 -0800

    update msi script to handle pre release

--- package-msi.bat
+++ package-msi.bat
@@ -1,12 +1,16 @@
-:: GH-14 This script uses jpackage to create an msi installer
-:: make sure to set the version number
+:: GH-14 This script uses jpackage w wix 3.14 to create a msi installer
+:: wix toolset 3.14 is available here:
+:: https://github.com/wixtoolset/wix3/releases/tag/wix3141rtm
 
-set VERSION=0.5.1
+:: make sure to set the version number
+set VERSION=0.6.0-dev.e
+set MSI_VER=0.6.0
+
 set JPKG_EXE="C:\Program Files\java\jdk-21\bin\jpackage"
 
 del jDiskMark-*.msi
-%JPKG_EXE% --type msi --input jdiskmark-v%VERSION% --main-jar jDiskMark.jar ^
-    --name jDiskMark-%VERSION% --app-version %VERSION% ^
+%JPKG_EXE% --type msi --input jdiskmark-%VERSION% --main-jar jDiskMark.jar ^
+    --name jDiskMark-%VERSION% --app-version %MSI_VER% ^
     --vendor "jdiskmark.net" --win-console --win-menu
 
-ren jDiskMark-%VERSION%-%VERSION%.msi jDiskMark-%VERSION%.msi
+ren jDiskMark-%VERSION%-%MSI_VER%.msi jDiskMark-%VERSION%.msi
