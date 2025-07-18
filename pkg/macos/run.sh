5be1010216c67b70e8039e040f370d22d028a4f4
Tyler <tylerlui@gmail.com>
2025-07-17 23:39:43 -0700

    #16 - Initial DMG installer working

---
+++ pkg/macos/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+#CLASSPATH="$CURRENT_DIR/JDiskMark.jar:$CURRENT_DIR/lib/*"
+
+#java -cp "$CLASSPATH" jdiskmark.App
+
+
+java -jar jdiskmark.jar
